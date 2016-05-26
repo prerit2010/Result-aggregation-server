e7db54e182cdfe3f6c90a141f7be29d418f35a84
Prerit Garg <prerit2010@gmail.com>
2016-05-26 23:28:06 +0530

    Created sample models and flask-migration

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db
+import datetime
+
+class UserSystemInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    system_dist = db.Column(db.String)
+    uname  = db.Column(db.String)
+    version = db.Column(db.String)
+    system_dist = db.Column(db.String)
+    system = db.Column(db.String)
+    machine = db.Column(db.String)
+    system_platform = db.Column(db.String) 
+    uname = db.Column(db.String)
+    version = db.Column(db.String)
+    python_version = db.Column(db.String)
+    workshop_id = db.Column(db.String)
+    email_id = db.Column(db.String)
+    create_time = db.Column(db.DateTime, default=db.func.now())
+
+class SuccessfulInstalls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    version = db.Column(db.String)
+    create_time = db.Column(db.DateTime, default=db.func.now())
---
+++ config.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/result_aggregation.db'
