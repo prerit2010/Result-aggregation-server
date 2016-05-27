eaeb7e10d8cee48901ad763ac06ba245a3df60d1
Prerit Garg <prerit2010@gmail.com>
2016-05-28 02:51:29 +0530

    changed to list comprehension

--- app/views.py
+++ app/views.py
@@ -6,39 +6,39 @@ from models import UserSystemInfo, SuccessfulInstalls, FailedInstalls
 @app.route('/installation_data/', methods=['POST'])
 def installation_data():
 
-    user_system_info = request.json.get('user_system_info', None)
-    successful_installs = request.json.get('successful_installs', None)
-    failed_installs = request.json.get('failed_installs', None)
+    user_system_info = request.json.get('user_system_info')
+    successful_installs = request.json.get('successful_installs')
+    failed_installs = request.json.get('failed_installs')
 
-    system_dist = user_system_info.get('system_dist', None)
-    uname  = user_system_info.get('uname', None)
-    version = user_system_info.get('version', None)
-    system = user_system_info.get('system', None)
-    machine = user_system_info.get('machine', None)
-    system_platform = user_system_info.get('system_platform', None)
-    uname = user_system_info.get('uname', None)
-    python_version = user_system_info.get('python_version', None)
-    workshop_id = user_system_info.get('workshop_id', None)
-    email_id = user_system_info.get('email_id', None)
+    system_dist = user_system_info.get('system_dist')
+    uname  = user_system_info.get('uname')
+    version = user_system_info.get('version')
+    system = user_system_info.get('system')
+    machine = user_system_info.get('machine')
+    system_platform = user_system_info.get('system_platform')
+    uname = user_system_info.get('uname')
+    python_version = user_system_info.get('python_version')
+    workshop_id = user_system_info.get('workshop_id')
+    email_id = user_system_info.get('email_id')
 
-    success_objects_list = []
-    for succ_install in successful_installs:
-        obj = SuccessfulInstalls(name=succ_install.get('name', None), \
-                version=succ_install.get('version', None))
-        success_objects_list.append(obj)
+    success_objects_list = [
+        SuccessfulInstalls(name=succ_install.get('name'),
+            version=succ_install.get('version')) 
+            for succ_install in successful_installs 
+    ]
 
-    failed_objects_list = []
-    for fail_install in failed_installs:
-        obj = FailedInstalls(name=fail_install.get('name', None),\
-                version=fail_install.get('version', None), \
-                error_description=fail_install.get('error_description', None), \
-                error_cause=fail_install.get('error_cause', None))
-        failed_objects_list.append(obj)
+    failed_objects_list = [
+        FailedInstalls(name=fail_install.get('name'), version=fail_install.get('version'),
+            error_description=fail_install.get('error_description'),
+            error_cause=fail_install.get('error_cause')) 
+            for fail_install in failed_installs 
+    ]
 
-    user_info = UserSystemInfo(system_dist=system_dist, python_version=python_version,\
-                            uname=uname, version=version, system=system, \
-                            machine=machine, system_platform=system_platform, \
-                            workshop_id=workshop_id, email_id=workshop_id)
+    user_info = UserSystemInfo(system_dist=system_dist, python_version=python_version,
+                    uname=uname, version=version, system=system,
+                    machine=machine, system_platform=system_platform,
+                    workshop_id=workshop_id, email_id=workshop_id)
+    
     user_info.successful_installs.extend(success_objects_list)
     user_info.failed_installs.extend(failed_objects_list)
     db.session.add(user_info)
