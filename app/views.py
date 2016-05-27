43209f597d13b37f77be6cef9b24de6df71a4c22
Prerit Garg <prerit2010@gmail.com>
2016-05-27 14:18:11 +0530

    Changed the request type for user_system_info to dictionary

--- app/views.py
+++ app/views.py
@@ -6,16 +6,19 @@ from models import UserSystemInfo
 @app.route('/installation_data/', methods=['POST'])
 def installation_data():
 
-    system_dist = request.json.get('system_dist', None)
-    uname  = request.json.get('uname', None)
-    version = request.json.get('version', None)
-    system = request.json.get('system', None)
-    machine = request.json.get('machine', None)
-    system_platform = request.json.get('system_platform', None)
-    uname = request.json.get('uname', None)
-    python_version = request.json.get('python_version', None)
-    workshop_id = request.json.get('workshop_id', None)
-    email_id = request.json.get('email_id', None)
+    user_system_info = request.json.get('user_system_info', None)
+
+    system_dist = user_system_info.get('system_dist', None)
+    uname  = user_system_info.get('uname', None)
+    version = user_system_info.get('version', None)
+    system = user_system_info.get('system', None)
+    machine = user_system_info.get('machine', None)
+    system_platform = user_system_info.get('system_platform', None)
+    uname = user_system_info.get('uname', None)
+    python_version = user_system_info.get('python_version', None)
+    workshop_id = user_system_info.get('workshop_id', None)
+    email_id = user_system_info.get('email_id', None)
+
     try:
         user_info = UserSystemInfo(system_dist=system_dist, python_version=python_version,\
                                 uname=uname, version=version, system=system, \
