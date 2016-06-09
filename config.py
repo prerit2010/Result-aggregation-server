4fb3f927de613055b8b8aa61eb9c646fde34f365
Prerit Garg <prerit2010@gmail.com>
2016-06-09 19:37:09 +0530

    Generic path for database

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/result_aggregation.db'
+from os.path import expanduser
+home = expanduser("~")
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + home + '/result_aggregation.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
