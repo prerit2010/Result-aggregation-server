5d502a336d03e1dd8fa759107422dae77e5f7858
Prerit Garg <prerit2010@gmail.com>
2016-07-18 00:36:38 +0530

    Added sample database for testing

--- manage.py
+++ manage.py
@@ -1,12 +1,27 @@
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from config import SQLALCHEMY_DATABASE_URI
+from config import SAMPLE_DATABASE_URI
 from app import application, db
+import sqlite3
  
 migrate = Migrate(application, db)
  
 manager = Manager(application)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def import_db():
+    print("\nImporting sample data....\n")
+    try:
+        con = sqlite3.connect(SAMPLE_DATABASE_URI)
+        f = open('sample.sql', 'r')
+        sql = f.read()
+        con.executescript(sql)
+        con.close()
+    except:
+        print("Could not connect to database")
+    else:
+        print("Sample data imported successfully")
  
 if __name__ == '__main__':
     manager.run()
