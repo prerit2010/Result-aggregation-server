f5aff963502382b94733c8cc12f069a560523e2d
Prerit Garg <prerit2010@gmail.com>
2016-07-20 21:02:52 +0530

    Add an option --overwrite to overwrite the existing data for sample database

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from config import SAMPLE_DATABASE_URI
 from app import application, db
-import sqlite3, sys
+import sqlite3, sys, argparse
  
 migrate = Migrate(application, db)
  
@@ -10,7 +10,13 @@ manager = Manager(application)
 manager.add_command('db', MigrateCommand)
 
 @manager.command
-def import_db():
+def import_db(overwrite=False):
+    """
+    Import sample data for testing using `import_db` command. This command also contains an
+    option `--overwrite`. In case the there is already some data in the database, user has
+    the option to overwrite the database using `import_db --overwrite` or `import_db -o`. 
+    """
+    
     tables = ['user_system_info', 'successful_installs', 'failed_installs' ,'attempts']
     try:
         con = sqlite3.connect(SAMPLE_DATABASE_URI)
@@ -20,11 +26,11 @@ def import_db():
     cursor = con.execute("SELECT * from user_system_info limit 1")
     result= cursor.fetchone()
     if result:
-        choice = input("Database already contains some data. Do you wish to delete all data and overwrite? (y/n) :")
-        if choice == 'y' or choice == 'Y':
+        if overwrite:
             for table in tables:
                 con.execute("DELETE FROM " + table)
         else:
+            print("\nDatabase already contains some data. To overwrite the existing data, please use the option --overwrite")
             sys.exit(0)
     print("\nImporting sample data....\n")
     with open('sample.sql', 'r') as f:
