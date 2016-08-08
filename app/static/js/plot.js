1b7da4aafb53d086f9fe23339104aba7a9be8012
Prerit Garg <prerit2010@gmail.com>
2016-08-08 23:47:26 +0530

    Added direct label for linux distributions on '/view/detail/'

--- app/static/js/plot.js
+++ app/static/js/plot.js
@@ -304,6 +304,20 @@ function func_os_user_dist_for_package(system){
             type: 'bar'
         }
     ];
+
+    var annotations_content = [];
+    for( var i = 0 ; i < x_values.length ; i++ ){
+        var result = {
+            x: x_values[i],
+            y: y_values[i],
+            text: y_values[i],
+            xanchor: 'center',
+            yanchor: 'bottom',
+            showarrow: false
+        };
+        annotations_content.push(result);
+    }
+
     var layout = {
         title: '',
         showlegend: false,
@@ -312,7 +326,8 @@ function func_os_user_dist_for_package(system){
         },
         yaxis : {
             title : 'Number of users'
-        }
+        },
+        annotations: annotations_content
     };
     Plotly.newPlot('os_user_dist_by_package', data, layout);
 
