a9c7add715b31350807aa79c4f573a81298e2b6e
Prerit Garg <prerit2010@gmail.com>
2016-08-16 16:06:55 +0530

    Moved js to call.js

--- app/static/js/call.js
+++ app/static/js/call.js
@@ -1,4 +1,20 @@
 function SelectElement(valueToSelect)
 {    
-    document.getElementById('sel_workshop').value = valueToSelect;
+  document.getElementById('sel_workshop').value = valueToSelect;
+}
+
+function compare_button(){
+  if(document.getElementById('sel2').value != "None" && document.getElementById('sel1').value != "None"){
+    document.getElementById('submit_button').value = "Compare";
+    document.getElementById('submit_button').disabled = false;
+  }
+  else{
+    if(document.getElementById('sel1').value != "None"){
+      document.getElementById('submit_button').value = "Submit";
+      document.getElementById('submit_button').disabled = false;
+    }
+    else
+      document.getElementById('submit_button').disabled = true;
+  }
+  return true;
 }
