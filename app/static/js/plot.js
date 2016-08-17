b7fa70a3ca9bd54b35ade7d7751a585fcc849f31
Prerit Garg <prerit2010@gmail.com>
2016-08-17 22:53:26 +0530

    Added docstrings for plots written in javascript -- plot.js

--- app/static/js/plot.js
+++ app/static/js/plot.js
@@ -1,3 +1,12 @@
+/* 
+This file contails all the graphs that are plotted using plotly.js
+*/
+
+
+/* There are global setting applied to each plot. These involve
+ * removal some button provided by plotly.js by default, and also
+ * display the mode bar permanenly for each plot.
+*/
 var mode_bar_global_settings = {
     modeBarButtonsToRemove: [
         'sendDataToCloud',
@@ -11,6 +20,9 @@ var mode_bar_global_settings = {
 
 
 function func_os_user(os_user){
+    /* Plot a graph for different operating systems used by users on '/view/'
+    */
+
     var x_values = []
     var y_values = []
     for(var i = 0, size = os_user.length; i < size ; i++){
@@ -49,10 +61,14 @@ function func_os_user(os_user){
         },
         annotations: annotations_content
     };
-    Plotly.newPlot('os_user', data, layout, mode_bar_global_settings);
+    Plotly.newPlot('os_user', data, layout, mode_bar_global_settings); //applied global settings
 }
 
+
 function func_failed_installs_all(most_failed_packages){
+    /* Plot a bar graph for most failed packages (all) on '/view/'
+    */
+
     var x_values = []
     var y_values = []
     for(var i=0 ; i < most_failed_packages.length ; i++){
@@ -85,14 +101,19 @@ function func_failed_installs_all(most_failed_packages){
     Plotly.newPlot('failed_package', data, layout, mode_bar_global_settings);
 }
 
+
 function func_failed_installs(most_failed_packages){
+    /* Plot a bar graph for most failed packages and limit the results
+     * upto 10 only.
+    */
+
     var x_values = []
     var y_values = []
     for(var i=0 ; i < most_failed_packages.length ; i++){
         var failed_package = most_failed_packages[i]['name'] + ' (' + most_failed_packages[i]['version'].split('(')[0] + ')';
         x_values.push(failed_package);
         y_values.push(most_failed_packages[i]['count']);
-        if(i > 8){
+        if(i > 8){ // break as soon as top 10 packages are being added
             break;
         }
     }
@@ -135,7 +156,12 @@ function func_failed_installs(most_failed_packages){
     Plotly.newPlot('failed_package', data, layout, mode_bar_global_settings);
 }
 
+
 function func_failed_installs_names_all(most_failed_packages){
+    /* Plot a bar graph for most failed packages (all) grouped by their name only.
+     * Versions are not taken into account here. 
+    */
+
     var x_values = []
     var y_values = []
     for(var i=0 ; i < most_failed_packages.length ; i++){
@@ -167,7 +193,12 @@ function func_failed_installs_names_all(most_failed_packages){
     Plotly.newPlot('failed_package_names', data, layout, mode_bar_global_settings);
 }
 
+
 function func_failed_installs_names(most_failed_packages){
+    /* Plot a bar graph for most failed packages (top 10) grouped by their name only.
+     * Versions are not taken into account here. 
+    */
+
     var x_values = []
     var y_values = []
     for(var i=0 ; i < most_failed_packages.length ; i++){
@@ -216,7 +247,13 @@ function func_failed_installs_names(most_failed_packages){
     Plotly.newPlot('failed_package_names', data, layout, mode_bar_global_settings);
 }
 
+
 function func_python_users(python_version_one, python_version_two, package_name_one, package_name_two){
+    /* Plot a bar graph for different python versions that are used by users
+     * while executing the installation scripts. It accepts for 2 packages for comparison,
+     * and plots only for first package if the second one is not selected for comparison.
+    */
+
     var x_values = [];
     var y_values = [];
     for(var i = 0, size = python_version_one.length; i < size ; i++){
@@ -229,7 +266,7 @@ function func_python_users(python_version_one, python_version_two, package_name_
         name: package_name_one,
         type: 'bar'
     };
-    if(python_version_two){
+    if(python_version_two){ // only if second package is selected for comparison
         var x_values = [];
         var y_values = [];
         for(var i = 0, size = python_version_two.length; i < size ; i++){
@@ -273,10 +310,15 @@ function func_python_users(python_version_one, python_version_two, package_name_
         annotations: annotations_content
     };
     Plotly.newPlot('python_users', data, layout, mode_bar_global_settings);
-
 }
 
+
 function func_os_user_for_package(os_users_one, os_users_two, package_name_one, package_name_two){
+    /* Plot a bar graph for different operating systems that are used by users
+     * while executing the installation scripts. It accepts for 2 packages for comparison,
+     * and plots only for first package if the second one is not selected for comparison.
+    */
+
     var x_values = [];
     var y_values = [];
     for(var i = 0, size = os_users_one.length; i < size ; i++){
@@ -289,7 +331,7 @@ function func_os_user_for_package(os_users_one, os_users_two, package_name_one, 
         name: package_name_one,
         type: 'bar'
     };
-    if(os_users_two){
+    if(os_users_two){ // only if second package is selected for comparison
         var x_values = [];
         var y_values = [];
         for(var i = 0, size = os_users_two.length; i < size ; i++){
@@ -333,11 +375,17 @@ function func_os_user_for_package(os_users_one, os_users_two, package_name_one, 
         annotations: annotations_content
     };
     Plotly.newPlot('os_user_by_package', data, layout, mode_bar_global_settings);
-
 }
 
 
 function func_os_user_dist_for_package(linux_dist_package_one, linux_dist_package_two, package_name_one, package_name_two){
+    /* Plot a bar graph for different Linux distriubtions that are used by users
+     * while executing the installation scripts. It accepts for 2 packages for comparison,
+     * and plots only for first package if the second one is not selected for comparison.
+     * This graph is not plotted (function is not called) if there is no linux user for this
+     * package
+    */
+
     var x_values = [];
     var y_values = [];
     for(var i = 0, size = linux_dist_package_one.length; i < size ; i++){
@@ -395,11 +443,17 @@ function func_os_user_dist_for_package(linux_dist_package_one, linux_dist_packag
         annotations: annotations_content
     };
     Plotly.newPlot('os_user_dist_by_package', data, layout, mode_bar_global_settings);
-
 }
 
 
 function func_failed_package_time_series(create_time_package_one, create_time_package_two, package_name_one, package_name_two){
+    /* Plot a line chart for the failed package to view the frequency of failure of this package
+     * over the period of time. It accepts for 2 packages for comparison,
+     * and plots only for first package if the second one is not selected for comparison.
+     * This graph is not plotted (function is not called) if results are filted by a specific
+     * workshop.
+    */
+
     var x_values = [];
     var y_values = [];
     for (var key in create_time_package_one) {
@@ -456,5 +510,4 @@ function func_failed_package_time_series(create_time_package_one, create_time_pa
         annotations: annotations_content
     };
     Plotly.newPlot('create_time', data, layout, mode_bar_global_settings);
-
 }
